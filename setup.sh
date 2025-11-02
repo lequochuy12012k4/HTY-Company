ab4936bdb27096dc7426af827d8267a96c4c5f3b
quochuy120124 <quochuy120124@gmail.com>
2025-11-02 02:53:12 +0000

    Oke

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 echo "Cloning repository..."
 git clone https://github.com/lequochuy12012k4/HTY-Company
+echo "Changing directory to HTY-Company..."
+cd HTY-Company
+echo "setup virtual enviroment"
+python -m venv .venv
+source .venv/bin/activate
 echo "Installing Python requirements..."
 pip install -r requirements.txt
 echo Migrate to database
